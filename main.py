f74712c54ac94f701908d6b392adf8e469cf9918
Jonfor <jonforscher@gmail.com>
2016-05-04 19:28:46 -0400

    CSV stuff

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
+try:
+    import unicodecsv as csv
+except ImportError:
+    import warnings
+
+    warnings.warn("can't import `unicodecsv` encoding errors may occur")
+    import csv
+
 import requests
 from bs4 import BeautifulSoup
-#      https://www.google.com/search?q=tesla&biw=1440&bih=802&source=lnt&tbs=cdr%3A1%2Ccd_min%3A1%2F1%2F2013%2Ccd_max%3A1%2F5%2F2013&tbm=nws
-URL = 'https://www.google.com/search?&q={query}&biw=1440&bih=802&source=lnt&tbs=cdr%3A1%2Ccd_min%3A1%2F1%2F{year}%2Ccd_max%3A1%2F5%2F{year}&tbm=nws'
+
+URL = 'https://www.google.com/search?&q={query}&biw=1440&bih=802&source=lnt&tbs=cdr%3A1%2Ccd_min%3A{month}%2F{from_day}%2F{year}%2Ccd_max%3A{month}%2F{to_day}%2F{year}&tbm=nws'
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36'}
 
+
 def run(**params):
     response = requests.get(URL.format(**params), headers=headers)
-    # 'https://www.google.com/search?&q=tesla&biw=1440&bih=802&source=lnt&tbs=cdr%3A1%2Ccd_min%3A1%2F1%2F2013%2Ccd_max%3A1%2F5%2F2013&tbm=nws'
-    html_doc = response.content
-    # FIND H3 TAGS WITH EITHER r OR _U6c CLASSES
-    soup = BeautifulSoup(html_doc, 'html.parser')
 
+    html_doc = response.content
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    tesla_csv = 'tesla.csv'
+    with open(tesla_csv, 'wb') as csvfile:
+        writer = csv.writer(csvfile, delimiter="|")
+        writer.writerow(['title', 'link', 'from_date', 'to_date'])
+        for anchor in soup.find_all('h3'):
+            for link in anchor.find_all('a'):
+                from_date = str(params.get('month')) + '/' + str(params.get('from_day')) + '/' + str(params.get('year'))
+                to_date = str(params.get('month')) + '/' + str(params.get('to_day')) + '/' + str(params.get('year'))
+                writer.writerow([link.getText(), link.get('href'), from_date, to_date])
+                print(link.getText())
 
 if __name__ == "__main__":
-    run(query="tesla", month=3, from_day=2, to_day=3, year=2013)
+    run(query="tesla", month=1, from_day=1, to_day=5, year=2013)
